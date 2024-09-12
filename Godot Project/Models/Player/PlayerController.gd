127676f5940edad53bb00de125f2793690cd741e
RXCodes <RXPlaysGames@gmail.com>
2024-09-11 21:31:53 -0400

    add backwards walking animation

--- Godot Project/Models/Player/PlayerController.gd
+++ Godot Project/Models/Player/PlayerController.gd
@@ -1,17 +1,43 @@
 extends Node2D
 
-var playerSpeed = 5
+var playerSpeed = 3.5
 
 # Called when the player enters the scene tree for the first time.
 func _ready() -> void:
 	$MainAnimationPlayer.play("Idle")
 
 # Called every physics tick.
+var walking = false
+var walkingBackwards = false
 func _physics_process(delta: float) -> void:
 	# player movement
 	if currentMovementKeypresses.size() > 0:
 		var movementVector = currentMovementKeypresses[0]
-		position += movementVector * playerSpeed
+		
+		# walking animations
+		var currentlyWalkingBackwards = movementVector.x < 0
+		var shouldChangeAnimation = false
+		if not walking:
+			shouldChangeAnimation = true
+			walking = true
+		if not shouldChangeAnimation:
+			shouldChangeAnimation = currentlyWalkingBackwards != walkingBackwards
+		walkingBackwards = currentlyWalkingBackwards
+		if shouldChangeAnimation: 
+			if walkingBackwards:
+				$MainAnimationPlayer.play("Walk Backwards")
+			else:
+				$MainAnimationPlayer.play("Walk")
+			
+		# move player
+		if walkingBackwards:
+			position += movementVector * playerSpeed * 0.6
+		else:
+			position += movementVector * playerSpeed
+	else:
+		if walking:
+			$MainAnimationPlayer.play("Idle")
+			walking = false
 
 # keep track of which movement keys are being pressed
 var currentMovementKeypresses: Array = []
