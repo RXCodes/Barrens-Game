a7276610f2920ba7237da28445393b2572c6dd71
RXCodes <RXPlaysGames@gmail.com>
2024-12-01 07:25:38 -0500

    reanimate blue slimes

--- Godot Project/Models/slime_enemy.gd
+++ Godot Project/Models/slime_enemy.gd
@@ -6,23 +6,18 @@ var is_player_in_range: bool = false
 
 # Function called when the enemy spawns into the scene
 func onStart() -> void:
-	if Player.current != null:
-		setTarget(Player.current, 60)
-		# Loop this while the enemy is alive
-		while not dead:
-			$ColliderBox/FlipTransform/Animations.play("Idle")
-			walkMovementSpeed = 2  # Slow speed when approaching the player
-			await enemyReachedTarget
-			# Make the enemy attack while in range of the player
-			while withinRangeOfTarget():
-				# Face towards the player
-				faceTarget()
-				walkMovementSpeed = 2  # Medium speed when attacking
-				# Play an attack animation
-				$ColliderBox/FlipTransform/Animations.play("Attack")
-				# Use the hurtbox to deal damage to the player
-				await TimeManager.wait(.7)
-				var damage = randf_range(2, 4)  # Random float from 6 to 10
-				activateHurtBox($ColliderBox/Hurtbox, damage, HurtBoxType.PLAYER)
-				# Little delay before base slime attacks again
-				await TimeManager.wait(.7)
+	setTarget(Player.current, 100)
+	while not dead:
+		walkMovementSpeed = 2  # Slow speed when approaching the player
+		await enemyReachedTarget
+		# Make the enemy attack while in range of the player
+		while withinRangeOfTarget():
+			faceTarget()
+			walkMovementSpeed = 0  
+			mainAnimationPlayer.stop()
+			playAnimation("Attack")
+			await TimeManager.wait(.7)
+			activateHurtBox($ColliderBox/Hurtbox,  randf_range(3, 5), HurtBoxType.PLAYER)
+			await enemyAnimationFinished
+			await TimeManager.wait(0.02)
+		await TimeManager.wait(0.02)
